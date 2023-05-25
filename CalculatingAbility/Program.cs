32f883f19d5b0f01043908b4049d806617fdf0c9
chajus1 <chajus1@googlemail.com>
2023-05-25 22:33:18 +0100

    Added AbilityScoreCalculator Class populated program.cs

--- CalculatingAbility/Program.cs
+++ CalculatingAbility/Program.cs
@@ -10,6 +10,28 @@ namespace CalculatingAbility
     {
         static void Main(string[] args)
         {
+            AbilityScoreCalculator calculator = new AbilityScoreCalculator();
+            while (true)
+            {
+                calculator.RollResult = ReadInt(calculator.RollResult, "Starting 4d6 roll");
+                calculator.DivideBy = ReadDouble(calculator.DivideBy, "Divide by");
+                calculator.AddAmount = ReadInt(calculator.AddAmount, "Add amount");
+                calculator.CalculateAbilityScore();
+                Console.WriteLine("Calculated ability score: " + calculator.Score);
+                Console.WriteLine("Press Q to quit, any other key to continue");
+                char keyChar = Console.ReadKey(true).KeyChar;
+                if ((keyChar == 'Q') || (keyChar == 'q')) return;
+            }
+        }
+
+        private static double ReadDouble(double divideBy, string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static int ReadInt(int rollResult, string v)
+        {
+            throw new NotImplementedException();
         }
     }
 }
